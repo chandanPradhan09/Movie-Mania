485eb40f9c88dac9f40802f84c210d19fbb74e2d
Chandan <chandan09112001@gmail.com>
2023-06-12 10:33:39 +0530

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGeneres } from "./store/homeSlice";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Eplore";
